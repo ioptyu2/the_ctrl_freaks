7cefa2ce2e5aba0f2466de88981ac2e9455c6570
ioptyu2 <mbatta1996@hotmail.com>
2023-06-12 11:44:53 +0100

    installed npm packages

---
+++ server/app.js
@@ -0,0 +1,10 @@
+const cors = require("cors")
+// import json files goes here
+const express = require("express")
+const app = express()
+// const logger = require("./logger")
+// app.use(logger)
+
+
+
+module.exports = app
