49c306d4ecea348d5ba200569f0fb59abbbed6c3
ioptyu2 <mbatta1996@hotmail.com>
2023-06-12 13:00:49 +0100

    Added some basic get requests

--- server/app.js
+++ server/app.js
@@ -1,18 +1,31 @@
 const cors = require("cors")
-// import json files goes here
-const easy = require("./easyQuestions.json")
-const medium = require("./mediumQuestions.json")
-const hard = require("./hardQuestions.json")
 const express = require("express")
 const app = express()
+
 // const logger = require("./logger")
 // app.use(logger)
 
-app.use("/easyQuestions", express.json())
-app.use("/mediumQuestions", express.json())
-app.use("/hardQuestions", express.json())
+const easy = require("./easyQuestions.json")
+const medium = require("./mediumQuestions.json")
+const hard = require("./hardQuestions.json")
 
+// app.use("/easyQuestions", express.json())
+// app.use("/mediumQuestions", express.json())
+// app.use("/hardQuestions", express.json())
 
+app.use(cors())
+app.use(express.json())
 
+app.get("/easyQuestions", (req, res) => {
+    res.send(easy)
+})
+
+app.get("/mediumQuestions", (req, res) => {
+    res.send(medium)
+})
+
+app.get("/hardQuestions", (req, res) => {
+    res.send(hard)
+})
 
 module.exports = app
