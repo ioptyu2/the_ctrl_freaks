9d561381025051786827d3f92ab6bd8b68bf4344
ioptyu2 <mbatta1996@hotmail.com>
2023-06-12 11:56:53 +0100

    importing json files

--- server/app.js
+++ server/app.js
@@ -1,9 +1,17 @@
 const cors = require("cors")
 // import json files goes here
+const easy = require("./easyQuestions.json")
+const medium = require("./mediumQuestions.json")
+const hard = require("./hardQuestions.json")
 const express = require("express")
 const app = express()
 // const logger = require("./logger")
 // app.use(logger)
+
+app.use("/easyQuestions", express.json())
+app.use("/mediumQuestions", express.json())
+app.use("/hardQuestions", express.json())
+
 
 
 
