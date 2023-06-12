47341e4a88cacb4f9def9ffe418f34e741ca328a
amalburhan <amalbrhn@gmail.com>
2023-06-12 15:31:56 +0100

    Add basic styling to quiz page

---
+++ client/assets/script.js
@@ -0,0 +1,49 @@
+const questions = {
+    "questions": [
+        {
+            "question": "Which of the following is NOT a renewable resource?",
+            "options": [
+                {
+                    "option": "Solar energy",
+                    "correct": false
+                },
+                {
+                    "option": "Wind energy",
+                    "correct": false
+                },
+                {
+                    "option": "Fossil fuels",
+                    "correct": true
+                },
+                {
+                    "option": "Hydroelectric power",
+                    "correct": false
+                }
+            ]
+        },
+        {
+            "question": "What is the capital city of Australia?",
+            "options": [
+                {
+                    "option": "Sydney",
+                    "correct": false
+                },
+                {
+                    "option": "Melbourne",
+                    "correct": false
+                },
+                {
+                    "option": "Canberra",
+                    "correct": true
+                },
+                {
+                    "option": "Perth",
+                    "correct": false
+                }
+            ]
+        }
+    ]
+}
+
+
+
