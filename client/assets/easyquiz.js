ca80ec40c5026216e64a4a2e6f6245039bca50f6
amalburhan <amalbrhn@gmail.com>
2023-06-13 17:14:45 +0100

    Fix play again button

--- client/assets/easyquiz.js
+++ client/assets/easyquiz.js
@@ -2,6 +2,9 @@
 const questionElement = document.getElementById("question");
 const answerElement = document.querySelector("#answer-buttons");
 const nextButton = document.getElementById("next-btn");
+const resultElement = document.getElementById("result");
+const playElement = document.getElementById("play");
+
 let questionData = null;
 let currentQuestionIndex = 0;
 let score = 0;
@@ -9,12 +12,13 @@ let score = 0;
 
 async function fetchQuestions() {
     try {
-        const response = await fetch('http://localhost:3005/questions');
+        const response = await fetch('http://localhost:3005/questions/easy');
         if (!response.ok) {
             console.log(`Error fetching JSON:`, response.status);
         }
         const data = await response.json();
         questionData = data;
+        console.log(questionData);
     } catch (err) {
         console.log(err);
     }
@@ -24,9 +28,11 @@ const showQuestion = () => {
 
     // Removes all previous buttons (if any) to create new buttons
     answerElement.innerHTML = "";
+    resultElement.textContent = "";
+    playElement.innerHTML = "";
 
     // Adds current question
-    const currentQuestion = questionData.questions[currentQuestionIndex];
+    const currentQuestion = questionData[currentQuestionIndex];
     console.log(currentQuestion);
     const questionNo = currentQuestionIndex + 1;
     questionElement.innerHTML = `${questionNo}. ${currentQuestion.question}`;
@@ -40,12 +46,13 @@ const showQuestion = () => {
         answerElement.appendChild(button);
         button.addEventListener("click", selectAnswer);
     });
+
 };
 
 const selectAnswer = (e) => {
     const selectedBtn = e.target;
     const selectedOptionIndex = parseInt(selectedBtn.dataset.index); // Uses index data attribute
-    const currentQuestion = questionData.questions[currentQuestionIndex];
+    const currentQuestion = questionData[currentQuestionIndex];
     const selectedOption = currentQuestion.options[selectedOptionIndex];
 
     if (selectedOption.correct) {
@@ -64,20 +71,34 @@ const selectAnswer = (e) => {
     nextButton.disabled = false;
 };
 
+const restartQuiz = () => {
+    currentQuestionIndex = 0;
+    score = 0;
+    showQuestion();
+    questionElement.style.display = "block";
+    answerElement.style.display = "block";
+    nextButton.style.display = "block";
+}
+
+
 const nextBtn = () => {
     currentQuestionIndex++;
 
-    if (currentQuestionIndex < questionData.questions.length) {
+    if (currentQuestionIndex < questionData.length) {
         showQuestion();
-        const nextButton = document.getElementById("next-btn");
         nextButton.disabled = true;
     } else {
-        const resultElement = document.getElementById("result");
-        resultElement.innerHTML = `You scored ${score} out of ${questionData.questions.length}!`;
+        resultElement.innerHTML = `You scored ${score} out of ${questionData.length}!`;
+        const playButton = document.createElement("button");
+        playButton.textContent = "Play again?";
+        playButton.setAttribute("id", "next-btn");
+        playElement.appendChild(playButton);
 
         questionElement.style.display = "none";
         answerElement.style.display = "none";
         nextButton.style.display = "none";
+
+        playButton.addEventListener("click", restartQuiz);
     }
 };
 
